04d1d8f9cdcacb1c52071f8d14bde7d04b92c967
Adi <akumatha@gmail.com>
2022-11-25 09:22:32 +0530

    p

---
+++ home.php
@@ -0,0 +1,227 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['usermail'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+
+<style>
+* {box-sizing: border-box}
+body {font-family: Verdana, sans-serif; margin:0}
+.mySlides {display: none}
+img {width: 100%;
+height:100%;
+}
+
+/* Slideshow container */
+.slideshow-container {
+  /* max-width: 1000px; */
+  position: relative;
+  margin: auto;
+}
+
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active, .dot:hover {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .prev, .next,.text {font-size: 11px}
+}
+
+#img_width{
+ width:100rem;
+ height:30rem;
+}
+</style>
+<body>
+    
+<!-- add data -->
+
+
+
+<ul>
+<li><a href="">Welcome to Tour&Travels Places</a></li>
+  <li><a href="home.php">Home</a></li>
+  <li><a href="header.php">About</a></li>
+  <li><a href="contact.php">Contact</a></li>
+  <!-- admin page  -->
+  <li><a href="admin_page.php">Admin CRUD</a></li>
+
+  <li style="float:right"><a class="active" href="login_form.php">Login</a></li>
+  <li style="float:right"><a class="active" href="register_form.php">Register</a></li>
+  <li style="float:right"><a class="active" href="logout.php">Logout</a></li>
+</ul>
+
+<!-- close data -->
+
+
+<!-- cursole Display -->
+
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img id="img_width" src="https://cdn.pixabay.com/photo/2017/03/05/00/34/panorama-2117310__480.jpg" style="width:100%" />  
+  <div class="text">Hotals Beach </div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img id="img_width" src="https://cdn.pixabay.com/photo/2017/12/16/22/23/regensburg-3023439_1280.jpg" style="width:100%">
+  <div class="text">Cool Place</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img id="img_width" src="https://cdn.pixabay.com/photo/2018/02/21/10/16/train-station-3169964_1280.jpg" style="width:100%">
+  <div class="text">Train Journy </div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+<!-- endcursole Dispaly  -->
+
+
+
+<div class="container">
+   <div class="content">
+
+
+       
+   <span><?php echo $_SESSION['usermail']; ?></span></p>
+
+    
+      <a href="logout.php" class="logout">logout</a>
+     
+   </div>
+
+</div> 
+
+
+<script>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
+
+</body>
+</html>
